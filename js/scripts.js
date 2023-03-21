d8fa01382865031f6001b798c45ce41ff3910b81
Emma Gerigscott <elgerig@gmail.com>
2023-03-21 13:48:38 -0700

    add logic and form to convert celcius to farenheit
    Co-authored-by: Hernan Verar <hernanverar@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 
-let celciusForm = document.getElementById('tempCelcius')
+let celciusForm = document.getElementById('tempCelcius');
 celciusForm.addEventListener("submit", (e) => {
-  e.preventDefault()
-  const display = document.querySelector('.celcius')
-  let userInput = document.getElementById('farenheit')
-  display.innerHTML = celcius(userInput.value) + " Degrees Celcius"
-})
+  e.preventDefault();
+  const celciusDisplay = document.querySelector('.celcius');
+  let userInput = document.getElementById('farenheit');
+  celciusDisplay.innerHTML = celcius(userInput.value) + " Degrees Celcius";
+});
+
+let farForm = document.getElementById('tempF');
+farForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const farDisplay = document.querySelector('.farenheit');
+  let userInput = document.getElementById('celcius');
+  farDisplay.innerHTML = farenheit(userInput.value) + " Degrees Farenheit";
+});
 
 function celcius(farenheit) {
   let celsius = (farenheit - 32) * 5/9;
   return parseInt(celsius);
 }
+
+function farenheit(c) {
+ let farenheit = (c * 9/5) + 32;
+ return parseInt(farenheit);
+}
+
