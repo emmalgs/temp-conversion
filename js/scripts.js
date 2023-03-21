b7aa1cd5de952449cfbedd2d7fa8fa9c9075fde3
Emma Gerigscott <elgerig@gmail.com>
2023-03-21 10:23:20 -0700

    add function to convert user input from farenheit to celcius and display in javascript
    Co-authored-by: Hernan Verar <hernanverar@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-// funciton that converts farenheit to celcius
-// variable that stores user input number
-// function that displays inputted number converted to celcius
+
+let celciusForm = document.getElementById('tempCelcius')
+celciusForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const display = document.querySelector('.celcius')
+  let userInput = document.getElementById('farenheit')
+  display.innerHTML = celcius(userInput.value) + " Degrees Celcius"
+})
+
+function celcius(farenheit) {
+  let celsius = (farenheit - 32) * 5/9;
+  return parseInt(celsius);
+}
